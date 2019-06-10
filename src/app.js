7a4e0a3d944567ad947d291ad03cd355a1de2315
rotiroti <rafael.rotiroti@inceres.com.br>
2019-06-10 09:49:51 -0300

    avançando...

--- src/app.js
+++ src/app.js
@@ -1,6 +1,16 @@
 var app = angular.module('app', []);
 
 app.controller('Semaforo', ['$scope', function($scope) {
+  // Cada farol verde deve durar 15 segundos
+  // Cada farol amarelo deve durar 3 segundos
+  // Cada farol vermelho deve durar 10 segundos
+  $scope.tempoVermelho = 10000;
+  $scope.tempoAmarelo = 3000;
+  $scope.tempoVerde = 15000;
+
+  $scope.tempoSemaforo1 = 28000;
+  $scope.tempoSemaforo2 = 28000;
+  // Os valores devem ser enviados em milisegundos
 
   $scope.semaforo1 = {
      vermelho: false,
@@ -13,53 +23,40 @@ app.controller('Semaforo', ['$scope', function($scope) {
     amarelo: false, 
     verde: false
   }
-
-  // Cada farol verde deve durar 15 segundos
-  // Cada farol amarelo deve durar 3 segundos
-  // Cada farol vermelho deve durar 10 segundos
+  //calcular o tempo total que os semaforos vão ficar ativos,
+  //usar setInteverval para determinar e dentro dele usar o setTimeout pois as luzes vao acender apenas uma vez durante o intervalo.
 
    //setTimeout(function, tempo) //a funcao setTimout roda apenas uma vez
      //setInterval(function, tempo) //a funcao setTimout roda sempre
-   setInterval(farol, 5000)
-   /***   
-      Se SEMAFORO1.VERMELHO === TRUE
-          SEMAFORO2.VERDE = TRUE
-          SEMAFORO2.AMARELO = FALSE
-          SEMAFORO2.VERMELHO = FALSE
+      //clearInterval(function) // para pausar a função
+      //clearTImeout(function) // para pausar a função
+   setInterval(semaforo1, $scope.tempoSemaforo1);
 
-          SEMAFORO1.AMARELO = FALSE
-          SEMAFORO1.VERDE = FALSE
-          SEMAFORO1.VERMELHO = FALSE
-
-          SEMAFORO2.TEMPO = 15
-
-      semaforo2.verde....
-      DEPOIS QUE ACABAR OS 15 SEGUNDOS:
-          SEMAFORO2.AMARELO = TRUE
-          SEMAFORO2.VERDE = FALSE
-          SEMAFORO2.VERMELHO = FALSE
-
-          SEMAFORO1.VERDE = FALSE
-          SEMAFORO1.AMARELO = FALSE
-          SEMAFORO1.VERMELHO = TRUE
-      
-      semaforo2.amarelo...
-      DEPOIS QUE ACABAR 3 SEGUNDOS:
-          SEMAFORO2.AMARELO = FALSE
-          SEMAFORO2.VERMELHO = FALSE
-          SEMAFORO2.VERDE = FALSE
-          
-          SEMAFORO1.VERDE = TRUE
-          SEMAFORO1.AMARELO = FALSE
-          SEMAFORO1.VERMELHO = FALSE
-   ***/
-   function farol() {
-     $scope.semaforo1.vermelho = true
-     if ($scope.semaforo1.vermelho === true) {
-       $scope.semaforo2.verde = true;
-     }
-     console.log($scope.semaforo1, $scope.semaforo2);
+   function semaforo1(){
+     setTimeout(vermelho, $scope.tempoVermelho);
    }
+   
+   function vermelho(){
+      $scope.semaforo1.vermelho = true;
+      $scope.semaforo1.amarelo = false;
+      $scope.semaforo1.verde = false;
+      console.log('Agora é Vermelho',$scope.semaforo1)
+      setTimeout(amarelo, $scope.tempoAmarelo);
+   }
+   function amarelo(){
+      $scope.semaforo1.vermelho = false;
+      $scope.semaforo1.amarelo = true;
+      $scope.semaforo1.verde = false;
+      console.log('Agora é Amarelo',$scope.semaforo1)
+      setTimeout(verde, $scope.tempoVerde);
+   }
+   function verde(){
+      $scope.semaforo1.vermelho = false;
+      $scope.semaforo1.amarelo = false;
+      $scope.semaforo1.verde = true
+      console.log('Agora é Verde',$scope.semaforo1)
+   }
+   
    console.log('Semaforo 1', $scope.semaforo1);
    console.log('Semaforo 2', $scope.semaforo2);
 }]);
