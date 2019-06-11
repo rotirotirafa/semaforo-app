afea759ae082c47e5f41898f1eb536efb9b97f2f
rotiroti <rafael.rotiroti@inceres.com.br>
2019-06-11 09:01:42 -0300

    adicionando função de countdown

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ app.controller('Semaforo', ['$scope', '$timeout', '$interval', function($scope, 
   // Cada farol amarelo deve durar 3 segundos
   // Cada farol vermelho deve durar 10 segundos
   $scope.tempoVermelho = 10;
-  $scope.tempoAmarelo = 13;
+  $scope.tempoAmarelo = 5;
   $scope.tempoVerde = 15;
 
   $scope.tempoSemaforo1 = 28;
@@ -13,16 +13,27 @@ app.controller('Semaforo', ['$scope', '$timeout', '$interval', function($scope, 
   // Os valores devem ser enviados em milisegundos, ou seja na hora de chamar a função deve ser feito como: 1000 * segundos
 
   $scope.semaforo1 = {
+     tempoTotal: $scope.tempoSemaforo1,
      vermelho: false,
      amarelo: false, 
      verde: false
    }
 
    $scope.semaforo2 = {
-    vermelho: false,
-    amarelo: false, 
-    verde: false
+     tempoTotal: $scope.tempoSemaforo2,  
+     vermelho: false,
+     amarelo: false, 
+     verde: false
+  }  
+
+  function tempoRestante(){
+     //receber tempo restante de cara farol
+   $scope.countDown = 10;    
+   $interval(function(){
+      console.log($scope.countDown--)
+   },1000,$scope.countDown);
   }
+  tempoRestante()
 
   function ContarSegundos(){
     var segundos = 5;
@@ -58,18 +69,21 @@ app.controller('Semaforo', ['$scope', '$timeout', '$interval', function($scope, 
       $scope.semaforo1.vermelho = true;
       $scope.semaforo1.amarelo = false;
       $scope.semaforo1.verde = false;
+      $scope.$apply();
       console.log('Agora é Vermelho',$scope.semaforo1)
    }
    function amarelo(){
       $scope.semaforo1.vermelho = false;
       $scope.semaforo1.amarelo = true;
       $scope.semaforo1.verde = false;
+      $scope.$apply();
       console.log('Agora é Amarelo',$scope.semaforo1)
    }
    function verde(){
       $scope.semaforo1.vermelho = false;
       $scope.semaforo1.amarelo = false;
-      $scope.semaforo1.verde = true
+      $scope.semaforo1.verde = true;
+      $scope.$apply();
       console.log('Agora é Verde',$scope.semaforo1)
    }
 }]);
